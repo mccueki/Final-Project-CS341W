f4524577bba689aecbb85989398feee70797ebd1
Karlie McCue <123039174+mccueki@users.noreply.github.com>
2024-11-26 00:34:03 -0800

    reactified

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      My balance
+        $921.48
+
+        Spending - Last 7 days
+
+        mon
+        tue
+        wed
+        thu
+        fri
+        sat
+        sun
+
+        Total this month
+        $478.33
+
+        +2.4%
+        from last month
+      </div>
+      
+    </>
+  )
+}
+
+export default App
